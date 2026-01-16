5a5ddec63d64b95ea7a2b01eea6ae61ba9903a3d
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-01-16 21:25:39 +0430

    the login page is done

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,11 @@
-import axios from './../node_modules/axios/lib/axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
-
+  baseURL: "http://127.0.0.1:8000/api",
+  headers: {
+    Accept: "application/json",
+  },
+  withCredentials: true,
 });
 
 export default api;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 
-import api from '../axios';
+// import api from '../../../../both1/both/karmands/axios';
+import api from "axios";
 
 const Login = () => {
 
